9f6787f722d0d6f225327a9bfb0897659356f4ae
Kyna Prettyman <ceona7@gmail.com>
2014-03-22 17:07:52 -0600

    updated styles and added comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322165313) do
+ActiveRecord::Schema.define(version: 20140322220949) do
+
+  create_table "comments", force: true do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "idea_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ideas", force: true do |t|
     t.string   "name"
